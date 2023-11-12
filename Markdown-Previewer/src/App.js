13cad60f9d65df738e8e9533edbd8345d6ff364f
Matt McMeans <mrmcmans@memphis.edu>
2023-11-11 20:32:01 -0600

    Added markdown parsing method

--- Markdown-Previewer/src/App.js
+++ Markdown-Previewer/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useState } from 'react';
+import { marked } from 'marked';
 
 function App() {
+
+  const [text, setText] = useState(`
+  # Heading (H1 Size) 
+  ## Subheading (H2 Size) 
+  [Visit OpenAI](https://www.openai.com/) 
+  Use backticks to highlight \`inline code\`.
+  - This is a list item
+  > This is a blockquote.
+  ![Alt Text](https://example.com/image.jpg)
+  **This text is bold.**`)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id="editor" onChange={(event) => {
+        setText(event.target.value)
+        }}
+        value={text}
+        placeholder='
+        ** Hello'
+      ></textarea>
+      <div id="preview"
+      dangerouslySetInnerHTML={{
+        __html: marked(text),
+      }}></div>
     </div>
   );
 }
